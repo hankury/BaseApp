6c8c25acf52aed91fc080c5eddbf405dfe284687
Hai <hankury@gmail.com>
2018-08-16 14:05:43 +0700

    Base

--- app/src/main/java/kirakira/com/baseapp/ui/base/BaseActivity.java
+++ app/src/main/java/kirakira/com/baseapp/ui/base/BaseActivity.java
@@ -1,11 +1,53 @@
 package kirakira.com.baseapp.ui.base;
 
+import android.app.ProgressDialog;
 import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.LayoutRes;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 
-public class BaseActivity extends AppCompatActivity implements BaseFragment.Callback {
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
+import kirakira.com.baseapp.utils.CommonUtils;
+import kirakira.com.baseapp.utils.NetworkUtils;
+import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
+
+public abstract class BaseActivity extends AppCompatActivity implements BaseFragment.Callback {
+    private ProgressDialog mProgressDialog;
+    private Unbinder binder;
+
+    public abstract
+    @LayoutRes
+    int getLayoutId();
+
+
+    public void hideKeyboard() {
+        View view = this.getCurrentFocus();
+        if (view != null) {
+            InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+            if (imm != null) {
+                imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+            }
+        }
+    }
+
+    public boolean isNetworkConnected() {
+        return NetworkUtils.isNetworkConnected(getApplicationContext());
+    }
+
+    public void hideLoading() {
+        if (mProgressDialog != null && mProgressDialog.isShowing()) {
+            mProgressDialog.cancel();
+        }
+    }
+
+    public void showLoading() {
+        hideLoading();
+        mProgressDialog = CommonUtils.showLoadingDialog(this);
+    }
 
     @Override
     public void onFragmentAttached() {
@@ -17,13 +59,21 @@ public class BaseActivity extends AppCompatActivity implements BaseFragment.Call
 
     }
 
-    public void hideKeyboard() {
-        View view = this.getCurrentFocus();
-        if (view != null) {
-            InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
-            if (imm != null) {
-                imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
-            }
-        }
+    @Override
+    protected void attachBaseContext(Context newBase) {
+        super.attachBaseContext(CalligraphyContextWrapper.wrap(newBase));
+    }
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(getLayoutId());
+        binder = ButterKnife.bind(this);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        binder.unbind(); //release ressource
     }
 }
--- app/src/main/java/kirakira/com/baseapp/ui/base/BaseFragment.java
+++ app/src/main/java/kirakira/com/baseapp/ui/base/BaseFragment.java
@@ -1,9 +1,77 @@
 package kirakira.com.baseapp.ui.base;
 
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.LayoutRes;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
-public class BaseFragment extends Fragment {
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
+
+public abstract class BaseFragment extends Fragment {
     private BaseActivity mActivity;
+    private Unbinder binder;
+
+    public abstract
+    @LayoutRes
+    int getLayoutId();
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof BaseActivity) {
+            BaseActivity activity = (BaseActivity) context;
+            this.mActivity = activity;
+            activity.onFragmentAttached();
+        }
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(false);
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View mRootView = null;
+        if (mRootView == null) {
+            mRootView = LayoutInflater.from(container.getContext()).inflate(getLayoutId(), container, false);
+            binder = ButterKnife.bind(this, mRootView);
+        }
+        return mRootView;
+    }
+
+    @Override
+    public void onDetach() {
+        mActivity = null;
+        super.onDetach();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        init(view, savedInstanceState);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if (binder != null) {
+            binder.unbind();
+        }
+    }
+
+    protected abstract void init(View view, Bundle savedInstanceState);
+
+    public BaseActivity getBaseActivity() {
+        return mActivity;
+    }
 
     public interface Callback {
 
